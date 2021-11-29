5d2b2d81b174ff2f5b366590e6639f958f14a7d1
Nils Bergmann <nilsbergmann@noim.io>
2021-11-29 17:57:16 +0100

    fix: remove now removed old system import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export * as system from './modules/system-old';
-export * as vm from './modules/vms/vm-module';
 export * as ssh from './executors/SSH';
+export * as vm from './modules/vms/vm-module';
