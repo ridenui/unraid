428e489d344f3be4d9cbe7d8e3f5719871ed87f1
Tom <me@dunklestoast.de>
2021-11-30 21:23:29 +0100

    feat(system): add loadaverage extension

--- examples/basic.ts
+++ examples/basic.ts
@@ -16,7 +16,7 @@ import { Unraid, UnraidConfig } from '../src/instance/unraid';
     },
   } as UnraidConfig<SSHConfig, SSHExecutor>);
 
-  const lsusb = await unraid.system.uptime();
+  const lsusb = await unraid.system.loadAverage();
 
   console.log(lsusb);
 
