a475498cd57b02ec12797574cfb216947e0a40fe
Nils Bergmann <nilsbergmann@noim.io>
2021-12-01 23:25:14 +0100

    feat(type): export all interfaces and types for the executor class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { Unraid } from './instance/unraid';
 import { Executor } from './instance/executor';
 
 export * from './executors/index';
+export * as Executor from './instance/executor';
 export { Unraid, Executor };
