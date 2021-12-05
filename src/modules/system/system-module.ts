9c3ca62b8d2d49d5538ddea35f28ccee9d13c730
Nils Bergmann <nilsbergmann@noim.io>
2021-12-05 17:44:19 +0100

    feat(system): add load average event stream

--- src/modules/system/system-module.ts
+++ src/modules/system/system-module.ts
@@ -2,6 +2,7 @@ import { mix } from 'ts-mixer';
 import { Executor } from '../../instance/executor';
 import { Unraid } from '../../instance/unraid';
 import { EventExtension } from '../extension/event-extension';
+import type { ILoadAverage, ILoadAverageStreamOption } from './extensions';
 import {
   SystemModuleDateExtension,
   SystemModuleDiskfree,
@@ -19,7 +20,14 @@ import {
 
 // required for mixins
 export interface SystemModule<ExecutorConfig, Ex extends Executor<ExecutorConfig>>
-  extends EventExtension<ExecutorConfig, Ex, { syslog: string }>,
+  extends EventExtension<
+      ExecutorConfig,
+      Ex,
+      {
+        syslog: { handlerType: string };
+        loadAverage: { handlerType: ILoadAverage; optionType: ILoadAverageStreamOption };
+      }
+    >,
     SystemModuleDateExtension<ExecutorConfig, Ex>,
     SystemModuleDiskfree<ExecutorConfig, Ex>,
     SystemModuleHostnameExtension<ExecutorConfig, Ex>,
