a9fe4fcb729cfcb1e2353c59ef3c8c57857a4d81
Nils Bergmann <nilsbergmann@noim.io>
2022-01-15 15:46:23 +0100

    refactor(notifications): improve load time of notifications

---
+++ examples/notification_test.ts
@@ -0,0 +1,32 @@
+/*
+    This is a very basic example. Make sure to set your environment correctly.
+    If you don't know how, check out the README.md :)
+ */
+import { SSHConfig, SSHExecutor } from '../src/executors';
+import { Unraid, UnraidConfig } from '../src/instance/unraid';
+
+(async () => {
+  const unraid = new Unraid({
+    executor: SSHExecutor,
+    executorConfig: {
+      host: process.env.SSH_HOST,
+      password: process.env.SSH_PASSWORD,
+      username: process.env.SSH_USER,
+      port: parseInt(process.env.SSH_PORT ?? '22', 10),
+    },
+  } as UnraidConfig<SSHConfig, SSHExecutor>);
+
+  const hostname = await unraid.system.getHostname();
+
+  console.log(hostname);
+
+  const notificationCount = await unraid.unraid.getNotificationCount();
+
+  console.log(notificationCount);
+
+  const notifications = await unraid.unraid.getNotifications();
+
+  console.log(notifications);
+
+  unraid.executor.disconnect();
+})();
